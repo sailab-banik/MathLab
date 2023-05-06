65d3ebdcea7619a65017e8d3b2609dd1fec496b3
Sailab <sailabbanik24@gmail.com>
2023-05-06 22:42:23 +0530

    Add Search Compose

---
+++ app/src/main/java/com/sailab/mathlab/core/presentation/search/Search.kt
@@ -0,0 +1,71 @@
+package com.sailab.mathlab.core.presentation.search
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardVoice
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Search() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        var text by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(28.dp)),
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            singleLine = true,
+            placeholder = {
+                Text(text = "Search")
+            },
+            leadingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search"
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardVoice,
+                        contentDescription = "Voice")
+                }
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+
+                }
+            )
+        )
+    }
+}
--- app/src/main/java/com/sailab/mathlab/feature_home/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/sailab/mathlab/feature_home/presentation/home/HomeScreen.kt
@@ -1,18 +1,16 @@
 package com.sailab.mathlab.feature_home.presentation.home
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.sailab.mathlab.core.presentation.search.Search
 
 @Composable
 fun HomeScreen() {
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
     ) {
-        Text(text = "Home")
+        Search()
     }
 }
